9496f95c67e00fb812ad35febe647a4228995cde
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-09 18:23:07 -0600

    Ejercicios de lab4

---
+++ Laboratorio4_A03252/backend_lab_A03252/backend_lab_A03252/Controllers/CountryController.cs
@@ -0,0 +1,26 @@
+﻿using backend_lab_A03252.Models;
+using backend_lab_A03252.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_A03252.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly CountryService countryService;
+
+        public CountryController()
+        {
+            countryService = new CountryService();
+        }
+
+        [HttpGet]
+        public List<CountryModel> Get()
+        {
+            var countries = countryService.GetCountries();
+            return countries;
+        }
+    }
+}
---
+++ Laboratorio4_A03252/backend_lab_A03252/backend_lab_A03252/Repositories/CountryRepository.cs
@@ -0,0 +1,24 @@
+﻿using backend_lab_A03252.Models;
+using Dapper;
+//using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace backend_lab_A03252.Repositories
+{
+    public class CountryRepository
+    {
+        private readonly string _connectionString;
+        public CountryRepository()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _connectionString = builder.Configuration.GetConnectionString("CountryContext");
+        }
+
+        public List<CountryModel> GetCountries()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            string query = "SELECT * FROM dbo.Country";
+            return connection.Query<CountryModel>(query).ToList();
+        }
+    }
+}
