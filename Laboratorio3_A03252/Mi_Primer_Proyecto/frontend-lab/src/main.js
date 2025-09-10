3fdbfaa8ee67eb15656f1b8d4fb20c96421fc9aa
Anabelle <anabelle.porras@ucr.ac.cr>
2025-09-09 20:34:04 -0600

    Laboratorio #5: configuracion del router

--- Laboratorio3_A03252/Mi_Primer_Proyecto/frontend-lab/src/main.js
+++ Laboratorio3_A03252/Mi_Primer_Proyecto/frontend-lab/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import CountryList from './components/CountryList.vue';
+import CountryForm from './components/CountryForm.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", name: "Home", component: CountryList},
+        { path: "/country", name: "Country", component: CountryForm},
+    ],
+});
+
+createApp(App).use(router).mount("#app");
